c8dd481f34d182176a7bc419411bd50f62a86e20
Petteri Mäntymaa <petteri.mantymaa@gmail.com>
2023-02-23 19:45:04 +0200

    fix linewidth

--- explore/explore_delay.R
+++ explore/explore_delay.R
@@ -3,9 +3,9 @@ source(here::here("R/readers.R"))
 delay <- read_olkiluoto_delay()
 
 delay.rep <- delay %>%
-  mutate(notified.diff = thlMisc::thlNa0(as.numeric(notified-lag(notified))),
+  mutate(notified.diff = replace_na(as.numeric(notified-lag(notified)), 0),
          time_total = cumsum(notified.diff),
-         est_completion.diff = thlMisc::thlNa0(as.numeric(est_completion-lag(est_completion))),
+         est_completion.diff = replace_na(as.numeric(est_completion-lag(est_completion)), 0),
          planned_delay = as.numeric(est_completion - notified),
          planned_delay.robust = 3*planned_delay,
          penalty = 1+log1p(time_total/planned_delay.robust),
@@ -13,6 +13,6 @@ delay.rep <- delay %>%
 
 
 ggplot(delay.rep) +
-  geom_segment(aes(x = notified, xend = est_completion, y = n, yend = n), lwd = 2, color = "grey") +
+  geom_segment(aes(x = notified, xend = est_completion, y = n, yend = n), linewidth = 2, color = "grey") +
   geom_segment(aes(x = notified, xend = est_completion.robust, y = n, yend = n)) +
   theme_minimal()
